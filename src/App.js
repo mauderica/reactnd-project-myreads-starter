30066702db969e9005ae0e95d3f252d89c48d153
Mauderica <maude.chalin@gmail.com>
2018-03-25 13:06:01 -0400

    docs: get books from API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 import SearchBooks from './SearchBooks'
 import ListBooks from './ListBooks'
@@ -102,6 +102,7 @@ const bookshelves = [
 
 class BooksApp extends React.Component {
   state = {
+    books: [],
     /**
      * TODO: Instead of using this state variable to keep track of which page
      * we're on, use the URL in the browser's address bar. This will ensure that
@@ -109,6 +110,12 @@ class BooksApp extends React.Component {
      * pages, as well as provide a good URL they can bookmark and share.
      */
     showSearchPage: false
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books })
+    })
   }
 
   showLibrary = () => this.setState({showSearchPage: false})
--- src/Book.js
+++ src/Book.js
@@ -1,6 +1,7 @@
-// Note: This component must receive a 'bookshelves', (about the book:) 'style', 'title', 'authors' prop in its call
+// Note: This component must receive a 'bookshelves', (about the book:) 'coverStyle', 'title', 'authors' prop in its call
 
 import React from 'react'
+import * as BooksAPI from './BooksAPI'
 import ShelfChanger from './ShelfChanger';
 
 class Book extends React.Component {
@@ -13,11 +14,22 @@ class Book extends React.Component {
         return (
             <div className="book">
                 <div className="book-top">
-                    <div className="book-cover" style={this.props.style}></div>
-                    <ShelfChanger bookshelves={this.props.bookshelves}/>
+                    <div className="book-cover" style={{
+                        width: 140,
+                        height: 200,
+                        backgroundImage: `url(${this.props.book.imageLinks.thumbnail})`,
+                    }}></div>
+                    {/* 
+                        TODO: The selected option of 'ShelfChanger' should be linked to the internal state of the book,
+                        much like a form input controlled component.
+                        
+                        TODO: The position of the checkmark should match the Book component state
+                        (idea: pass down this info to ShelfChanger as a prop?)
+                    */}
+                    <ShelfChanger bookshelves={this.props.bookshelves} onSelectShelf={this.updateShelf} />
                 </div>
-                <div className="book-title">{this.props.title}</div>
-                <div className="book-authors">{this.props.authors}</div>
+                <div className="book-title">{this.props.book.title}</div>
+                <div className="book-authors">{this.props.book.authors.join(', ')}</div>
             </div>
         )
     }
