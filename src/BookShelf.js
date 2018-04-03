07c49a5b2d87f68ec91b0a6f90e428e144c3f5e1
Mauderica <maude.chalin@gmail.com>
2018-04-03 15:33:14 -0400

    feat: move books between shelves

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,7 +1,7 @@
-// Note: This component must receive a 'title' prop in its call
+// TODO: add PropTypes
 
 import React from 'react'
-import Book from './Book.js'
+import Book from './Book';
 
 class BookShelf extends React.Component {
     render() {
@@ -10,8 +10,16 @@ class BookShelf extends React.Component {
                 <h2 className="bookshelf-title">{this.props.title}</h2>
                 <div className="bookshelf-books">
                     <ol className="books-grid">
-                        <li>
-                        </li>
+                        {this.props.books.map((book, index) => (
+                            book.shelf === this.props.optionValue &&
+                            (<li key={index}>
+                                <Book
+                                    book={book}
+                                    bookshelves={this.props.bookshelves}
+                                    onBookMove={(bookMoved) => this.props.onBookMove(bookMoved)}
+                                />
+                            </li>)
+                        ))}
                     </ol>
                 </div>
             </div>
